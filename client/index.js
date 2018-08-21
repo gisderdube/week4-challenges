f69124778a0028e5aa8527eee3b3e2ad77559675
Cesar Vinken <cesarvinken@hotmail.com>
2018-08-21 18:20:46 +0200

    done

--- client/index.js
+++ client/index.js
@@ -1,1 +1,15 @@
-console.log('Let\'s get started!')
+console.log("Let's get started!");
+
+Promise.all([
+  axios.get("/randomword"),
+  axios.get("/randomword"),
+  axios.get("/randomword"),
+  axios.get("/randomword"),
+  axios.get("/randomword")
+]).then(responses => {
+  $("#ul-wrapper").append(`<ul></ul>`);
+  responses.map(res => {
+    $("#ul-wrapper ul").append(`<li>${res.data}</li>`);
+    console.log(res.data);
+  });
+});
--- server.js
+++ server.js
@@ -1,25 +1,34 @@
-const http = require('http')
-const fs = require('fs')
+const http = require("http");
+const fs = require("fs");
+const utils = require("./utils");
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        const htmlFile = fs.readFileSync('./client/index.html')
-        res.write(htmlFile)
-        res.end()
-    } else if (req.url === '/style.css') {
-        const stylesFile = fs.readFileSync('./client/style.css')
-        res.setHeader('Content-Type', 'text/css')
-        res.write(stylesFile)
-        res.end()
-    } else if (req.url === '/index.js') {
-        const jsFile = fs.readFileSync('./client/index.js')
-        res.write(jsFile)
-        res.end()
-    } else {
-        res.statusCode = 404
-        res.write('We could not find what you are looking for')
-        res.end()
-    }
-})
+  if (req.url === "/") {
+    const htmlFile = fs.readFileSync("./client/index.html");
+    res.write(htmlFile);
+    res.end();
+  } else if (req.url === "/style.css") {
+    const stylesFile = fs.readFileSync("./client/style.css");
+    res.setHeader("Content-Type", "text/css");
+    res.write(stylesFile);
+    res.end();
+  } else if (req.url === "/index.js") {
+    const jsFile = fs.readFileSync("./client/index.js");
+    res.write(jsFile);
+    res.end();
+  } else if (req.url === "/about") {
+    const aboutPage = fs.readFileSync("./client/about.html");
+    res.write(aboutPage);
+    res.end();
+  } else if (req.url === "/randomword") {
+    const randomWord = utils.createRandomWord();
+    res.write(randomWord);
+    res.end();
+  } else {
+    res.statusCode = 404;
+    res.write("We could not find what you are looking for");
+    res.end();
+  }
+});
 
-server.listen(3000)
+server.listen(3000);
